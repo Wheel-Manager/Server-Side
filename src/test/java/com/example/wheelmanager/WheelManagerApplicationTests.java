9b37e75b7cdfb97f3df1178f3d62664b48ab5d12
Hugo Arauco <araparhug7@gmail.com>
2021-05-11 13:17:19 -0500

    First commit

---
+++ src/test/java/com/example/wheelmanager/WheelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wheelmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
